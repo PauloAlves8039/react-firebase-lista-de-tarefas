25970b43f37ef836619b0411be0735c426894608
PauloAlves8039 <pj38alves@gmail.com>
2024-12-19 17:24:09 -0300

    feat: criado componente register e rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { db } from "./firebaseConnection";
+import { BrowserRouter } from 'react-router-dom'
+import RoutesApp from './routes'
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>Lista de Tarefas</h1>
-    </div>
+    <BrowserRouter>
+      <RoutesApp/>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/Register/index.jsx
@@ -0,0 +1,9 @@
+function Register() {
+    return (
+        <div>
+            <h1>Página Register</h1>
+        </div>
+    )
+}
+
+export default Register;
